2f3ad205d88899579ef1381f11ae19b82492bcae
Shalini-Raman <shaliniraman1999@gmail.com>
2026-03-23 15:02:55 +0530

    DynamicContent_Userwisetask

--- src/app/Tasks/tas/tas.ts
+++ src/app/Tasks/tas/tas.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+interface Task{
+  id:string;
+  userId:string;
+  title:string;
+  summary:string;
+  duedate:string;
 
+}
 @Component({
   selector: 'app-tas',
   standalone: true,
@@ -8,5 +15,5 @@ import { Component } from '@angular/core';
   styleUrl: './tas.css'
 })
 export class TasComponent {
-
+@Input({required:true}) tas!:Task;
 }
