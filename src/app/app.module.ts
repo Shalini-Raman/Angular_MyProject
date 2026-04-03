dfaf6e7bb260daf7ad2324654a597d61fb0bfe79
Shalini-Raman <shaliniraman1999@gmail.com>
2026-04-03 10:41:01 +0530

    SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import {AppComponent} from './app';
 import { HeaderComponent } from './header/Header';
 import { UserComponent } from './user/user';
 import { TaskComponent } from './Tasks/Task';
-import { CardComponent } from './shared/card/card';
+
 import { TasComponent } from './Tasks/tas/tas';
 import { NewTaskComponent } from './Tasks/new-task/new-task';
-
+import {SharedModule} from './shared/shared.module';
 @NgModule({
-    declarations : [AppComponent,HeaderComponent,UserComponent,CardComponent,TaskComponent,TasComponent,NewTaskComponent],
+    declarations : [AppComponent,HeaderComponent,UserComponent,TaskComponent,TasComponent,NewTaskComponent],
     bootstrap:[AppComponent],
-    imports:[BrowserModule,FormsModule]
+    imports:[BrowserModule,FormsModule,SharedModule]
 })
 export class AppModule{
 
